11c460f271bde21c7ada041204cc552f37b50c04
Rodrigo Cassanheira <a21703091@alunos.ulht.pt>
2018-11-23 11:48:01 +0000

    Fiz o CrazyPiece e o Tabuleiro

--- src/pt/ulusofona/lp2/crazyChess/CrazyPiece.java
+++ src/pt/ulusofona/lp2/crazyChess/CrazyPiece.java
@@ -1,4 +1,30 @@
 package pt.ulusofona.lp2.crazyChess;
 
 public class CrazyPiece {
+    int idPeca;
+    int idTipo;
+    int idEquipa;
+    String alcunha;
+    int x;
+    int y;
+
+    CrazyPiece(int idPeca, int idTipo, int idEquipa, String alcunha) {
+        this.idPeca = idPeca;
+        this.idTipo = idTipo;
+        this.idEquipa = idEquipa;
+        this.alcunha = alcunha;
+    }
+
+    void setCoordenadas(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    int getId() {
+        return idPeca;
+    }
+
+    String getImagePNG() {
+        return "";
+    }
 }
--- src/pt/ulusofona/lp2/crazyChess/Tabuleiro.java
+++ src/pt/ulusofona/lp2/crazyChess/Tabuleiro.java
@@ -1,4 +1,33 @@
 package pt.ulusofona.lp2.crazyChess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tabuleiro {
+    int tamanho;
+    List<CrazyPiece> pecas;
+    int turno;
+
+    Tabuleiro(int tamanho) {
+        this.tamanho = tamanho;
+        this.turno = 1;
+        this.pecas = new ArrayList<>();
+    }
+
+    public List<CrazyPiece> getPecas() {
+        return pecas;
+    }
+
+    public int getTamanho() {
+        return tamanho;
+    }
+
+    int getIdEquipaAJogar() {
+        if (turno % 2 == 0) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
 }
