71bcb586c16cdb9a3f5bde76b60e23b0170cd971
Francisco Madruga <41487452+FMadruga-21705328@users.noreply.github.com>
2018-11-23 11:51:35 +0000

    sfsdgfg

--- src/pt/ulusofona/lp2/crazyChess/Simulador.java
+++ src/pt/ulusofona/lp2/crazyChess/Simulador.java
@@ -1,4 +1,49 @@
 package pt.ulusofona.lp2.crazyChess;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Simulador {
+    Tabuleiro tabuleiro;
+
+    Simulador() {
+        this.tabuleiro = new Tabuleiro();
+    }
+
+    boolean iniciaJogo(File ficheiroInicial) {
+        try {
+            Scanner scanner = new Scanner(ficheiroInicial);
+            Map<Integer, CrazyPiece> pecas = new HashMap<>();
+            int tamanhoTabuleiro;
+            int numPecas = 0;
+            int numLinha = 0;
+            while (scanner.hasNextLine()) {
+                String linha = scanner.nextLine();
+                if (numLinha == 0) {
+                    tamanhoTabuleiro = Integer.parseInt(linha);
+                }
+                else if (numLinha == 1) {
+                    numPecas = Integer.parseInt(linha);
+                }
+                else {
+                    String dados [] = linha.split(":");
+                    if (numLinha < numPecas + 2) {
+                        CrazyPiece peca = new CrazyPiece(Integer.parseInt(dados[0]), Integer.parseInt(dados[1]), Integer.parseInt(dados[2]), dados[3]);
+                        pecas.put(Integer.parseInt(dados[0]), peca);
+                    }
+                    else {
+
+                    }
+                }
+                numLinha++;
+            }
+            scanner.close();
+        }
+        catch (FileNotFoundException exception) {
+            return false;
+        }
+    }
 }
