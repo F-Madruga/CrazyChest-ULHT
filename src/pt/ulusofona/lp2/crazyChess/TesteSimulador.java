a29cb9db3055b4c10c8389f74d5a902e3947a77d
Rodrigo Cassanheira <a21703091@alunos.ulht.pt>
2018-11-28 22:11:24 +0000

    outro teste e xlm

--- src/pt/ulusofona/lp2/crazyChess/TesteSimulador.java
+++ src/pt/ulusofona/lp2/crazyChess/TesteSimulador.java
@@ -1,7 +1,6 @@
 package pt.ulusofona.lp2.crazyChess;
 
 import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -73,9 +72,29 @@ public class TesteSimulador {
         reiHostil.setCoordenadas(1,0);
 
         assertTrue(simulador.processaJogada(0,0,1,0));
+        assertEquals(1, rei.getX());
+        assertEquals(0, rei.getY());
         assertEquals(-1, reiHostil.getX());
         assertEquals(-1, reiHostil.getY());
         assertEquals(1, simulador.gestor.getNumBrancas());
         assertEquals(0, simulador.gestor.getNumPretas());
+    }
+    @Test
+    public void test05ProcessaJogada(){
+        Simulador simulador = new Simulador();
+        CrazyPiece rei = new CrazyPiece(1,0, 1, "rei");
+        Tabuleiro tabuleiro = new Tabuleiro(4);
+        GestorDeJogo gestorDeJogo = new GestorDeJogo(0,1);
+
+        simulador.setGestor(gestorDeJogo);
+        simulador.setTabuleiro(tabuleiro);
+
+        simulador.gestor.setTurno(1);
+        simulador.tabuleiro.inserirPeca(rei);
+
+        rei.setCoordenadas(2,3);
+        assertTrue(simulador.processaJogada(2,3,3,2));
+        assertEquals(3, rei.getX());
+        assertEquals(2, rei.getY());
     }
 }
