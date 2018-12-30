f3210ec1ae6ef6afb458c68829e4b0bf68d75d58
Rodrigo Cassanheira <a21703091@alunos.ulht.pt>
2018-12-30 09:35:42 +0000

    movimentos do Cavalo Magico done

--- src/pt/ulusofona/lp2/crazyChess/PoneiMagico.java
+++ src/pt/ulusofona/lp2/crazyChess/PoneiMagico.java
@@ -21,7 +21,33 @@ public class PoneiMagico extends CrazyPiece{
         if ((this.x - xD == -2 && this.y - yD == -2) || (this.x - xD == -2 && this.y - yD == 2) || (this.x - xD == 2 && this.y - yD == -2) || (this.x - xD == 2 && this.y - yD == 2)) {
             int x = this.x;
             int y = this.y;
-
+                for (CrazyPiece peca : pecas){
+                    if (xD > x && yD < y) {
+                        if (peca.getIdTipo() == 0 && (peca.getX() == x && (peca.getY() == y - 1 || peca.getY() == y - 2)) || (peca.getX() == x + 1 && peca.getY() == y - 2)) {
+                            if (peca.getIdTipo() == 0 && ((peca.getX() == x + 1 || peca.getX() == x + 2) && peca.getY() == y) || (peca.getX() == x + 2 && peca.getY() == y - 1)) {
+                                return false;
+                            }
+                        }
+                    } else if (xD < x && yD < y) {
+                        if (peca.getIdTipo() == 0 && (peca.getX() == x && (peca.getY() == y - 1 || peca.getY() == y - 2)) || (peca.getX() == x - 1 && peca.getY() == y - 2)) {
+                            if (peca.getIdTipo() == 0 && ((peca.getX() == x - 1 || peca.getX() == x - 2) && peca.getY() == y) || (peca.getX() == x - 2 && peca.getY() == y - 1)) {
+                                return false;
+                            }
+                        }
+                    } else if (xD < x && yD > y) {
+                        if (peca.getIdTipo() == 0 && ((peca.getX() == x - 1 || peca.getX() == x - 2) && peca.getY() == y)|| (peca.getX() == x - 2 && peca.getY() == y + 1)){
+                            if (peca.getIdTipo() == 0 && (peca.getX() == x && (peca.getY() == y + 1 || peca.getY() == y + 2)) || (peca.getX() == x - 1 && peca.getY() == y + 2)) {
+                                return false;
+                            }
+                        }
+                    } else if (xD > x && yD > y){
+                        if(peca.getIdTipo() == 0 && (peca.getX() == x && (peca.getY() == y + 1 || peca.getY() == y + 2))|| (peca.getX() == x + 1 && peca.getY() == y + 2)){
+                            if(peca.getIdTipo() == 0 && ((peca.getX() == x + 1 || peca.getX() == x + 2) && peca.getY() == y) || (peca.getX() == x + 2 && peca.getY() == y + 1)){
+                                return false;
+                            }
+                        }
+                    }
+                }
             return true;
         } else {
             return false;
@@ -34,6 +60,13 @@ public class PoneiMagico extends CrazyPiece{
 
     public List<String> darSugestoes(List<CrazyPiece> pecas, int turno, int tamanho){
         List<String> sugestoes = new ArrayList<>();
+        for (int x = 0; x < tamanho; x++) {
+            for (int y = 0; y < tamanho; y++) {
+                if (verificarSeMove(x, y, pecas, turno)) {
+                    sugestoes.add(x + ", " + y);
+                }
+            }
+        }
         return sugestoes;
     }
 }
